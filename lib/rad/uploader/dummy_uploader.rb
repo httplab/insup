c73308df9d8e0e4ff525a7141dcdd479bc2ef18a
Nu-hin <nuinuhin@gmail.com>
2014-02-14 19:22:07 +0400

    Bugfixes

--- lib/rad/uploader/dummy_uploader.rb
+++ lib/rad/uploader/dummy_uploader.rb
@@ -3,7 +3,12 @@ class Rad::Uploader::DummyUploader < Rad::Uploader
   def upload_all files
     puts '---->Dummy Uploader<-----'
     files.each do |x|
-      puts "Uploading #{x}... Done."
+      case x.state
+      when Rad::TrackedFile::NEW, Rad::TrackedFile::MODIFIED
+        puts "Uploading #{x.path}... Done.".green
+      when Rad::TrackedFile::DELETED
+        puts "Deleting #{x.path}... Done.".red
+      end
     end
   end
 
