a98ee7cbf17c89112a80baad644c464910d3b3be
Nu-hin <nuinuhin@gmail.com>
2014-05-11 03:08:12 +0400

    Added help message on no command. Added license. Added some specs.

---
+++ spec/lib/insup/console/upload_observer_spec.rb
@@ -0,0 +1,20 @@
+require_relative '../../../spec_helper'
+
+describe 'Insup::Console::UploadObserver' do
+
+  it 'should handle all types of events' do
+    @observer = Insup::Console::UploadObserver.new
+
+    [Insup::Uploader::CREATING_FILE,
+    Insup::Uploader::CREATED_FILE,
+    Insup::Uploader::MODIFYING_FILE,
+    Insup::Uploader::MODIFIED_FILE,
+    Insup::Uploader::DELETING_FILE,
+    Insup::Uploader::DELETED_FILE,
+    Insup::Uploader::BATCH_UPLOADING_FILES,
+    Insup::Uploader::BATCH_UPLOADED_FILES].each do |event|
+      expect {@observer.update(event, Insup::TrackedFile.new('path/to.file'))}.not_to raise_error
+    end
+  end
+
+end
--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -3,3 +3,4 @@ require 'coveralls'
 Coveralls.wear!
 
 require_relative('../lib/insup')
+require_relative('../lib/insup/console')
